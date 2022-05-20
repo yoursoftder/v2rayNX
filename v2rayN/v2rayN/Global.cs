6e0f847161e0b7097623fb3639b309889fc0ea59
yoursoftder <89470006+yoursoftder@users.noreply.github.com>
2022-05-20 16:53:41 +0800

    Add files via upload

---
+++ v2rayN/v2rayN/Global.cs
@@ -0,0 +1,239 @@
+﻿
+namespace vNet
+{
+    class Global
+    {
+        #region 常量
+
+
+        public const string AboutUrl = @"https://github.com/2dust/v2rayN";
+        public const string UpdateUrl = AboutUrl + @"/releases";
+
+
+        /// <summary>
+        /// SpeedTestUrl
+        /// </summary>
+        public const string SpeedTestUrl = @"http://speedtest-sgp1.digitalocean.com/10mb.test";
+        public const string SpeedPingTestUrl = @"https://www.google.com/generate_204";
+
+        /// <summary>
+        /// CustomRoutingListUrl
+        /// </summary>
+        public const string CustomRoutingListUrl = @"https://raw.githubusercontent.com/2dust/v2rayCustomRoutingList/master/";
+
+        public const string GFWLIST_URL = "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt";
+
+        /// <summary>
+        /// PromotionUrl
+        /// </summary>
+        public const string PromotionUrl = @"aHR0cHM6Ly8xLjIzNDQ1Ni54eXovYWJjLmh0bWw=";
+
+        /// <summary>
+        /// 本软件配置文件名
+        /// </summary>
+        public const string ConfigFileName = "guiNConfig.json";
+
+        /// <summary>
+        /// v2ray配置文件名
+        /// </summary>
+        public const string v2rayConfigFileName = "config.json";
+
+        /// <summary>
+        /// v2ray客户端配置样例文件名
+        /// </summary>
+        public const string v2raySampleClient = "vNet.Sample.SampleClientConfig.txt";
+        /// <summary>
+        /// v2ray服务端配置样例文件名
+        /// </summary>
+        public const string v2raySampleServer = "vNet.Sample.SampleServerConfig.txt";
+        /// <summary>
+        /// v2ray配置Httprequest文件名
+        /// </summary>
+        public const string v2raySampleHttprequestFileName = "vNet.Sample.SampleHttprequest.txt";
+        /// <summary>
+        /// v2ray配置Httpresponse文件名
+        /// </summary>
+        public const string v2raySampleHttpresponseFileName = "vNet.Sample.SampleHttpresponse.txt";
+        /// <summary>
+        /// 空白的pac文件
+        /// </summary>
+        public const string BlankPacFileName = "vNet.Sample.BlankPac.txt";
+
+        public const string CustomRoutingFileName = "vNet.Sample.custom_routing_";
+
+
+        /// <summary>
+        /// 默认加密方式
+        /// </summary>
+        public const string DefaultSecurity = "auto";
+
+        /// <summary>
+        /// 默认传输协议
+        /// </summary>
+        public const string DefaultNetwork = "tcp";
+
+        /// <summary>
+        /// Tcp伪装http
+        /// </summary>
+        public const string TcpHeaderHttp = "http";
+
+        /// <summary>
+        /// None值
+        /// </summary>
+        public const string None = "none";
+
+        /// <summary>
+        /// 代理 tag值
+        /// </summary>
+        public const string agentTag = "proxy";
+
+        /// <summary>
+        /// 直连 tag值
+        /// </summary>
+        public const string directTag = "direct";
+
+        /// <summary>
+        /// 阻止 tag值
+        /// </summary>
+        public const string blockTag = "block";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string StreamSecurity = "tls";
+
+        public const string InboundSocks = "socks";
+        public const string InboundHttp = "http";
+        public const string Loopback = "127.0.0.1";
+        public const string InboundAPITagName = "api";
+        public const string InboundAPIProtocal = "dokodemo-door";
+
+
+        /// <summary>
+        /// vmess
+        /// </summary>
+        public const string vmessProtocol = "vmess://";
+        /// <summary>
+        /// shadowsocks
+        /// </summary>
+        public const string ssProtocol = "ss://";
+        /// <summary>
+        /// socks
+        /// </summary>
+        public const string socksProtocol = "socks://";
+        public const string trojanProtocol = "trojan://";
+        /// <summary>
+        /// http
+        /// </summary>
+        public const string httpProtocol = "http://";
+        /// <summary>
+        /// https
+        /// </summary>
+        public const string httpsProtocol = "https://";
+
+        /// <summary>
+        /// pac
+        /// </summary>
+        public const string pacFILE = "pac.txt";
+
+        /// <summary>
+        /// email
+        /// </summary>
+        public const string userEMail = "t@t.tt";
+
+        /// <summary>
+        /// MyRegPath
+        /// </summary>
+        public const string MyRegPath = "Software\\vNetNGUI";
+
+        /// <summary>
+        /// Language
+        /// </summary>
+        public const string MyRegKeyLanguage = "CurrentLanguage";
+        /// <summary>
+        /// Icon
+        /// </summary>
+        public const string CustomIconName = "Network.theme-dark.ico";
+
+        public enum StatisticsFreshRate
+        {
+            quick = 1000,
+            medium = 2000,
+            slow = 3000
+        }
+        public const string StatisticLogOverall = "StatisticLogOverall.json";
+
+        public const string IEProxyExceptions = "localhost;127.*;10.*;172.16.*;172.17.*;172.18.*;172.19.*;172.20.*;172.21.*;172.22.*;172.23.*;172.24.*;172.25.*;172.26.*;172.27.*;172.28.*;172.29.*;172.30.*;172.31.*;192.168.*";
+
+        #endregion
+
+        #region 全局变量
+
+        /// <summary>
+        /// 是否需要重启服务V2ray
+        /// </summary>
+        public static bool reloadV2ray
+        {
+            get; set;
+        }
+          
+        public static int addhttpport
+        {
+            get; set;
+        }
+        public static int addvmessport
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// 是否开启全局代理(http)
+        /// </summary>
+        public static bool sysAgent
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// socks端口
+        /// </summary>
+        public static int socksPort
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// http端口
+        /// </summary>
+        public static int httpPort
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// PAC端口
+        /// </summary>
+        public static int pacPort
+        {
+            get; set;
+        }
+
+        /// <summary>
+        ///  
+        /// </summary>
+        public static int statePort
+        {
+            get; set;
+        }
+
+        public static Job processJob
+        {
+            get; set;
+        }
+
+        #endregion
+
+
+
+    }
+}
---
+++ v2rayN/v2rayN/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+using vNet.Forms;
+using vNet.Properties;
+using vNet.Tool;
+
+namespace vNet
+{
+    static class Program
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] arg)
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                SetProcessDPIAware();
+            }
+
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+
+            //AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            Process instance = RunningInstance();
+            if (instance == null)
+            {
+                if (!UnzipLibs())
+                {
+                    UI.Show($"Error preparing the environment(准备运行环境出错)");
+                    return;
+                }
+
+                Utils.SaveLog("vN start up " + Utils.GetVersion());
+
+                //设置语言环境
+                string lang = Utils.RegReadValue(Global.MyRegPath, Global.MyRegKeyLanguage, "zh-Hans");
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(lang);
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm(arg));
+            }
+            else
+            {
+              //  UI.Show($"v2rayN is already running(v2rayN已经运行)");
+            }
+        }
+
+        //private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        //{
+        //    try
+        //    {
+        //        string resourceName = "v2rayN.LIB." + new AssemblyName(args.Name).Name + ".dll";
+        //        using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+        //        {
+        //            if (stream == null)
+        //            {
+        //                return null;
+        //            }
+        //            byte[] assemblyData = new byte[stream.Length];
+        //            stream.Read(assemblyData, 0, assemblyData.Length);
+        //            return Assembly.Load(assemblyData);
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        return null;
+        //    }
+        //}
+
+        /// <summary> 
+        /// 获取正在运行的实例，没有运行的实例返回null; 
+        /// </summary> 
+        public static Process RunningInstance()
+        {
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+            foreach (Process process in processes)
+            {
+                if (process.Id != current.Id)
+                {
+                    if (Assembly.GetExecutingAssembly().Location.Replace("/", "\\") == process.MainModule.FileName)
+                    {
+                        return process;
+                    }
+                }
+            }
+            return null;
+        }
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            Utils.SaveLog("Application_ThreadException", e.Exception);
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Utils.SaveLog("CurrentDomain_UnhandledException", (Exception)e.ExceptionObject);
+        }
+
+        static bool UnzipLibs()
+        {
+            var fileName = Utils.GetPath("libs.zip");
+            if (!FileManager.ByteArrayToFile(fileName, Resources.libs))
+            {
+                return false;
+            }
+
+            if (!FileManager.ZipExtractToFile(fileName))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
